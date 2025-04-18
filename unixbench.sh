370eff9fbbd06967b446329c99ab13984afb40b7
fudongwei.fdw <fudongwei.fdw@alibaba-inc.com>
2025-04-18 10:41:56 +0800

    fix issues on intel SPR and Synchronize commits from the community updates in recent years.

--- unixbench.sh
+++ unixbench.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #==============================================================#
-#   Description:  Unixbench script, copy from :https://raw.githubusercontent.com/teddysun/across/master/unixbench.sh, change to v5.1.6   #
+#   Description:  Unixbench script, copy from :https://raw.githubusercontent.com/teddysun/across/master/unixbench.sh, change to v5.1.7   #
 #   Author: Teddysun <i@teddysun.com>                          #
 #   Intro:  https://teddysun.com/245.html                      #
 #==============================================================#
@@ -25,18 +25,31 @@ fi
 mkdir -p ${cur_dir}
 cd ${cur_dir}
 
-# Download UnixBench5.1.6
-if [ -s UnixBench-5.1.6.tar.gz ]; then
-    echo "UnixBench-5.1.6.tar.gz [found]"
+# Download UnixBench5.1.7
+if [ -s UnixBench-5.1.7.tar.gz ]; then
+    echo "UnixBench-5.1.7.tar.gz [found]"
 else
-    echo "UnixBench-5.1.6.tar.gz not found!!!download now..."
-    if ! wget -c https://github.com/aliyun/byte-unixbench/releases/download/v5.1.6/UnixBench-5.1.6.tar.gz; then
-        echo "Failed to download UnixBench-5.1.6.tar.gz, please download it to ${cur_dir} directory manually and try again."
+    echo "UnixBench-5.1.7.tar.gz not found!!!download now..."
+    if ! wget -c https://github.com/aliyun/byte-unixbench/releases/download/v5.1.7/UnixBench-5.1.7.tar.gz; then
+        echo "Failed to download UnixBench-5.1.7.tar.gz, please download it to ${cur_dir} directory manually and try again."
         exit 1
     fi
 fi
-tar -zxvf UnixBench-5.1.6.tar.gz && rm -f UnixBench-5.1.6.tar.gz
-cd UnixBench-5.1.6/UnixBench
+tar -zxvf UnixBench-5.1.7.tar.gz && rm -f UnixBench-5.1.7.tar.gz
+cd UnixBench-5.1.7/UnixBench
+
+# 适配intel SPR/EMR等CPU型号无法识别march/mtune等问题出现的性能回退
+cpu_model=`lscpu | grep 'Model:' | awk '{print $2}'`
+gcc_version=$(gcc --version | head -n 1 | awk '{print $3}')
+major_version=$(echo $gcc_version | cut -d'.' -f1)
+# 判断 GCC 版本是否大于 5
+if [[ ${cpu_model} == 143 ]] || [[ ${cpu_model} == 207 ]]; then
+    if (( $major_version > 5 )); then
+        sed -i 's/OPTON += -march=native -mtune=native/OPTON += -march=icelake-server -mtune=icelake-server/g' Makefile
+    else
+        sed -i 's/OPTON += -march=native -mtune=native/OPTON += -march=core-avx2 -mtune=core-avx2/g' Makefile
+    fi
+fi
 
 #Run unixbench
 make
